780d2f2d15fcc11b826e7ecf687ec4e6bf3228d4
mizutayuto <yuto11752@i.softbank.jp>
2022-08-07 19:43:18 +0900

    second commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import { GetStaticProps } from 'next'
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
@@ -5,7 +6,7 @@ import { getSortedPostsData } from '../lib/posts';
 import Link from 'next/link';
 import Date from '../components/date';
 
-export async function getStaticProps() {
+export const getStaticProps: GetStaticProps = async() => {
   const allPostsData = getSortedPostsData();
   return {
     props: {
@@ -14,7 +15,14 @@ export async function getStaticProps() {
   };
 }
 
-export default function Home({ allPostsData }) {
+type Props = {
+  allPostsData: {
+    id: string
+    title: string
+    date: string
+  }[]
+}
+export default function Home({ allPostsData }: Props) {
   return (
     <Layout home>
       <Head>
